771fe6a718645d1e92659914bf21c57d055d2ae3
simonvennat <svennat@happy-garden.fr>
2025-03-29 21:17:10 +0100

    login API

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller;
 
+use App\Dto\TokenDto;
+use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'login')]
+    #[Route(path: '/login', name: 'login_admin')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
@@ -28,5 +35,34 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/api/token_check', methods: ['POST'])]
+    public function checkToken(
+        #[MapRequestPayload] TokenDto $token,
+        JWTEncoderInterface $JWTEncoder,
+        JWTTokenManagerInterface $JWTTokenManager,
+        UserRepository $userRepository,
+        SerializerInterface $serializer,
+    ): Response
+    {
+        try {
+            $decodedToken = $JWTEncoder->decode($token->token);
+
+            if ($decodedToken) {
+                $user = $userRepository->findOneBy(['username' => $decodedToken['username']]);
+                $token = $JWTTokenManager->create($user);
+
+                $tokenData = [
+                    'token' => $token,
+                    'user' => $serializer->normalize($user, null, ['groups' => ["user"]])
+                ];
+                return $this->json($tokenData);
+            }
+
+            throw new JWTDecodeFailureException(JWTDecodeFailureException::INVALID_TOKEN, 'Invalid token');
+        } catch (\Exception $e) {
+            return $this->json(['message' => $e->getMessage()], Response::HTTP_UNAUTHORIZED);
+        }
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
@@ -17,6 +18,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(['user'])]
     private ?string $username = null;
 
     /**
